73419c23c7640175bc551e73f803d684fa7aff69
srsanmartin97@gmail.com <srsanmartin97@gmail.com>
2021-07-11 22:44:58 -0300

    Falta ultimo punto de generar el batch

--- WinFormsApp2/Forms/7GestionPlanes.cs
+++ WinFormsApp2/Forms/7GestionPlanes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IsteaTVInternetCompany
 {
@@ -22,7 +23,112 @@ namespace IsteaTVInternetCompany
 
             buscarPlanesBox.TextChanged += BuscarPlanesBox_TextChanged;
             nuevoBtn.Click += NuevoBtn_Click;
+            cargarCSVButton.Click += CargarCSVButton_Click;
         }
+
+        private void CargarCSVButton_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = openFilePlanes.ShowDialog();
+
+            switch (resultado)
+            {
+                case DialogResult.OK:
+                    //MessageBox.Show(openFilePlanes.FileName);
+                    break;
+                case DialogResult.Cancel:
+                    break;                
+            }
+
+            List<Plan> registros = LeerPlanesDeCSV(openFilePlanes.FileName);
+
+            if (registros == null)
+            {
+                return;
+            }
+
+            foreach (Plan p in registros)
+            {
+                BaseDeDatos.Planes.Add(p);
+            }
+
+            RefrescarLista();
+        }
+
+        private List<Plan> LeerPlanesDeCSV(string path)
+        {
+            List<Plan> planes = new List<Plan>();
+
+            string[] registros = File.ReadAllLines(path);
+
+            planes.Capacity = registros.Length;
+
+            for (int i = 0; i < registros.Length; i++)
+            {
+                string[] campos = registros[i].Split(";");
+
+                Plan p = GenerarPlan(campos);
+
+                if(p == null)
+                {
+                    MessageBox.Show("Metiste cualquier data hermano");
+                    return null;
+                }
+                else
+                {
+                    planes.Add(p);
+                }
+            }
+            return planes;
+        }
+
+        private Plan GenerarPlan(string[] campos)
+        {
+            if (campos == null)
+            {
+                return null;
+            }
+
+            if (campos.Length != 4)
+            {                
+                return null;
+            }
+
+            //Nombre del plan; TV; Internet; Precio
+
+            string nombrePlan = campos[0];
+            string tv = campos[1];
+            string internet = campos[2];
+            string precio = campos[3];
+
+            decimal precioParseado;
+
+            Plan resultado = new Plan();
+
+            if (!decimal.TryParse(precio, out precioParseado))
+            {
+                return null;
+            }
+            if (nombrePlan.Length == 0)
+            {
+                return null;
+            }
+            if (tv.Length == 0)
+            {
+                return null;
+            }
+            if (internet.Length == 0)
+            {
+                return null;
+            }
+
+            resultado.Nombre = nombrePlan;
+            resultado.Tv = tv;
+            resultado.Precio = precioParseado;
+            resultado.Internet = internet;
+
+            return resultado;
+        }
+
         private void NuevoBtn_Click(object sender, EventArgs e)
         {
             AltaPlanes form = new AltaPlanes();
--- WinFormsApp2/Forms/8GestionProgramacion.cs
+++ WinFormsApp2/Forms/8GestionProgramacion.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace IsteaTVInternetCompany
 {
@@ -22,6 +23,98 @@ namespace IsteaTVInternetCompany
 
             buscarCanalesBox.TextChanged += BuscarCanalesBox_TextChanged;
             nuevoBtn.Click += NuevoBtn_Click;
+            cargarCSVButton.Click += CargarCSVButton_Click;
+        }
+
+        private void CargarCSVButton_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = openFileCanales.ShowDialog();
+
+            switch (resultado)
+            {
+                case DialogResult.OK:
+                    //MessageBox.Show(openFileCanales.FileName);
+                    break;
+                case DialogResult.Cancel:
+                    break;
+            }
+
+            List<Canal> registros = LeerCanalesDeCSV(openFileCanales.FileName);
+
+            if(registros == null)
+            {
+                return;
+            }
+
+            foreach (Canal c in registros)
+            {
+                BaseDeDatos.Canales.Add(c);
+            }
+
+            RefrescarLista();
+        }
+
+        private List<Canal> LeerCanalesDeCSV(string path)
+        {
+            List<Canal> canales = new List<Canal>();
+
+            string[] registros = File.ReadAllLines(path);
+
+            canales.Capacity = registros.Length;
+
+            for (int i = 0; i < registros.Length; i++)
+            {
+                string[] campos = registros[i].Split(";");
+
+                Canal c = GenerarCanal(campos);
+
+                if (c == null)
+                {
+                    MessageBox.Show("Metiste cualquier data hermano");
+                    return null;
+                }
+                else
+                {
+                    canales.Add(c);
+                }
+            }
+            return canales;
+        }
+
+        private Canal GenerarCanal(string[] campos)
+        {
+            if (campos == null)
+            {
+                return null;
+            }
+
+            if (campos.Length != 2)
+            {
+                return null;
+            }
+
+
+            string nombreCanal = campos[0];
+            string nroCanal = campos[1];
+
+            decimal nroCanalParseado;
+
+            Canal resultado = new Canal();
+
+            if (!decimal.TryParse(nroCanal, out nroCanalParseado))
+            {
+                return null;
+            }
+            if (nombreCanal.Length == 0)
+            {
+                return null;
+            }
+
+
+            resultado.Nombre = nombreCanal;
+            resultado.Nro = nroCanalParseado;
+            
+            return resultado;
         }
 
         private void NuevoBtn_Click(object sender, EventArgs e)
